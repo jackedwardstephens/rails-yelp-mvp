d2746d774367682e5cd836fb4714147305904a59
Jack Stephens <jack.stephens@me.com>
2022-11-16 15:49:25 +0000

    rspec green!

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
+  # get 'reviews/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # get "restaurants", to: "restaurants#index"
+  # get "restaurants/new", to: "restaurants#new"
+  # post "restaurants", to: "restaurants#create"
+  # get "restaurants/:id", to: "restaurants#show", as: :restaurant
+
+  # get "restaurants/:id/reviews/new" to: "reviews#new"
+  # post "restaurants/:id/reviews" to: "reviews#create"
+  resources :restaurants, only: [:new, :create, :show, :index] do
+    resources :reviews, only: [ :new, :create ]
+  end
 end
